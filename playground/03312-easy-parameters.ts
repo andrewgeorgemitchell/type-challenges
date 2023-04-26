0e52c4b91c6e85a502a422799ccf3d848556a3df
Andrew <andrewgeorgemitchell@gmail.com>
2023-04-25 22:48:45 -0700

    Complete parameters

--- playground/03312-easy-parameters.ts
+++ playground/03312-easy-parameters.ts
@@ -20,7 +20,11 @@
 
 /* _____________ Your Code Here _____________ */
 
-type MyParameters<T extends (...args: any[]) => any> = any
+type MyParameters<T extends (...args: any[]) => any> = T extends (
+  ...any: infer Props
+) => any
+  ? Props
+  : never
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
@@ -28,6 +32,8 @@ import type { Equal, Expect } from '@type-challenges/utils'
 const foo = (arg1: string, arg2: number): void => {}
 const bar = (arg1: boolean, arg2: { a: 'A' }): void => {}
 const baz = (): void => {}
+
+type te = MyParameters<typeof foo>
 
 type cases = [
   Expect<Equal<MyParameters<typeof foo>, [string, number]>>,
